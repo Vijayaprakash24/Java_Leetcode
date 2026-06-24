8ee01a1640f2615def606b2e1f61b0332a1e71b5
Vijayaprakash24 <vijayaprakash.m.v.s@gmail.com>
2026-06-24 15:41:12 +0530

    Time: 2 ms (83.55%), Space: 46.4 MB (91.57%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,7 +1,7 @@
 class Solution {
     List<List<String>> ans = new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
-        char[][] board = new char[n][n];
+       char[][] board = new char[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 board[i][j] = '.';
